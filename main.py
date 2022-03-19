0a1155a7e9ade8fe6f2929d563b9ace0e4bebb28
joannetang <joannetang2425@gmail.com>
2022-03-19 10:45:29 +0800

    update

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-import time
 import RepaymentNotice
 
-def setInterval(func, sec):
-    func()
-    time.sleep(sec)
-    setInterval(func, sec)
-
-try:
-    setInterval(RepaymentNotice.start, 86400)
-except Exception as e:
-    print('main error', e)
-
+RepaymentNotice.start()
